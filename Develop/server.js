4c11389ec42a39f2db14227b93975c03bd7a8baa
Jose Herrera <herrera.joserafael@gmail.com>
2023-09-12 00:30:16 -0400

    fix login path

--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-const sequelize = require('./config/connection');
+// const sequelize = require('./config/connection');
+// const seedAll = require('./seeds/index');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+// sequelize.sync({ force: false }).then(() => {
+//   console.log('Database synced.');
+//   seedAll().then(() => {
+//     console.log('Data seeded.');
+//     app.listen(PORT, () => {
+//       console.log(`App listening on port ${PORT}!`);
+//     });
+//   });
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
