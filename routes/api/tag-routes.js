b08d80d4ad930d99e8e8e228eb3fafe7d3a4eed9
Jose Herrera <herrera.joserafael@gmail.com>
2023-09-19 20:49:26 -0400

    fix my endpoints

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -61,6 +61,7 @@ router.put('/:id', async (req, res) => {
     
     res.status(200).json({ message: 'Tag updated' });
   } catch (err) {
+    console.log(err)
     res.status(500).json(err);
   }
 });
